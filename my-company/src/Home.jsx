67dc2499efe4c6a9080341eff8ad73f7dfb9b685
Andiswa Mnguni <andiswankanyiso.mnguni@gmail.com>
2025-09-09 18:09:15 +0200

    Build simple company website with React and React Router

---
+++ my-company/src/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Welcome to Our Company</h1>
+      <p>Your needs are our responsibility.</p>
+    </div>
+  );
+}
+
+export default Home;
