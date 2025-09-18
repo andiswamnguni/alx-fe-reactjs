027595638fefa7aec8e197bdccf88e46ac5a730c
Andiswa Mnguni <andiswankanyiso.mnguni@gmail.com>
2025-09-18 18:41:31 +0200

    Update githubService.js to use advanced search API with location and minRepos

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,18 @@
 // src/services/githubService.js
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
-  return response.data;
+/**
+ * Fetch GitHub users with advanced search criteria
+ * @param {Object} params - { username, location, minRepos }
+ * @returns {Promise<Object>} - GitHub API response { items: [...] }
+ */
+export const fetchUsers = async ({ username, location, minRepos }) => {
+  let query = username ? `${username} in:login` : "";
+  if (location) query += ` location:${location}`;
+  if (minRepos) query += ` repos:>=${minRepos}`;
+
+  const url = `https://api.github.com/search/users?q=${encodeURIComponent(query)}`;
+  
+  const response = await axios.get(url);
+  return response.data; // returns { items: [...] }
 };
