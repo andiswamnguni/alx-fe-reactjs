70ec167e3bb4cee7c2c06184ed12e86cb19326b9
Andiswa Mnguni <andiswankanyiso.mnguni@gmail.com>
2025-09-18 17:43:38 +0200

    Add components and services folders, update App.jsx with import

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,11 @@
 // src/App.jsx
+import React from "react";
+
 function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <p>Project setup complete 🎉</p>
+      <p>Setup complete 🎉</p>
     </div>
   );
 }
